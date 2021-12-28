57063eeb6c57b5f0985a2749aca63e985c97e633
Deun Lee <deunlee@deunlee.com>
2021-12-28 22:04:35 +0900

    Update first.sh

--- script/first.sh
+++ script/first.sh
@@ -2,20 +2,20 @@
 
 SCRIPT_DIR="$(dirname $(readlink -f $0))"
 
-# Colors
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
+NC="\033[0m"
+BLACK="\033[0;30m"  ; DGRAY="\033[1;30m"
+RED="\033[0;31m"    ; LRED="\033[1;31m"
+GREEN="\033[0;32m"  ; LGREEN="\033[1;32m"
+YELLOW="\033[0;33m" ; LYELLOW="\033[1;33m"
+BLUE="\033[0;34m"   ; LBLUE="\033[1;34m"
+PURPLE="\033[0;35m" ; LPURPLE="\033[1;35m"
+CYAN="\033[0;36m"   ; LCYAN="\033[1;36m"
+LGRAY="\033[0;37m"  ; WHITE="\033[1;37m"
 
-log_info()  { echo -e "\033[1;32m[INFO]\033[0m $*"  ; }
-log_warn()  { echo -e "\033[1;33m[WARN]\033[0m $*"  ; }
-log_error() { echo -e "\033[1;31m[ERROR]\033[0m $*" ; }
-log_debug() { echo -e "\033[1;35m[DEBUG]\033[0m $*" ; }
+log_info()  { echo -e "${LGREEN}[INFO]${NC} $*"   ; }
+log_warn()  { echo -e "${LYELLOW}[WARN]${NC} $*"  ; }
+log_error() { echo -e "${LRED}[ERROR]${NC} $*"    ; }
+log_debug() { echo -e "${LPURPLE}[DEBUG]${NC} $*" ; }
 # $@ is array, $* is string, $# is param count, $? is return value
 
 confirm() {
@@ -33,7 +33,7 @@ confirm() {
         *)    DEFAULT="";  PROMPT="$PROMPT[y/n] "   ;;
     esac
     while true; do
-        echo -en "\033[1;36m[CHECK]\033[0m " 1>&2
+        echo -en "${LCYAN}[CHECK]${NC} " 1>&2
         read -r -p "$PROMPT" INPUT
         case $INPUT in
             [Yy]|[Yy][Ee][Ss]) echo 'y'; break ;;
@@ -50,14 +50,18 @@ confirm() {
     done
 }
 
-trim() {
-    # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
+trim() { # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
     local var="$*"
     # Remove leading whitespace characters
     var="${var#"${var%%[![:space:]]*}"}"
     # Remove trailing whitespace characters
     var="${var%"${var##*[![:space:]]}"}"   
     printf '%s' "$var"
+}
+
+function_exists() { # https://stackoverflow.com/questions/85880/determine-if-a-function-exists-in-bash
+    declare -f -F $1 > /dev/null
+    return $?
 }
 
 get_distribution() {
@@ -71,10 +75,10 @@ get_distribution() {
 ################################################################################
 
 run_command() {
-    echo -en "\033[1;30m"
+    echo -en "$DGRAY"
     $@
     RET=$?
-    echo -en "\033[0m"
+    echo -en "$NC"
     return $RET
 }
 
@@ -97,7 +101,13 @@ update() {
 
 ################################################################################
 
+get_version() {
+    $1 --version | head -n 1
+}
+
 install() {
+    sudo true
+
     if check apt ; then
         run_command sudo apt install -y $@ | cat
     elif check yum ; then
@@ -119,56 +129,63 @@ install() {
 }
 
 install_package() { # install a package
-    PACKAGE="$1"
-    RECOMMEND="${2:-y}"
-    INSTALL_FUNC="${3:-install}"
+    name="$1"
+    dname="${2:-$name}" # display name
+    is_recommend="${3:-y}"
 
-    T=""
-    if check "$PACKAGE" ; then
-        T=" already"
-    elif [ $(confirm "Do you want to install $PACKAGE?" "$RECOMMEND") = "y" ]; then
-        $INSTALL_FUNC "$PACKAGE"
+    # Functions can be overridden.
+    func_check="${name}_check"
+    func_install="${name}_install"
+    func_version="${name}_version"
+    function_exists "$func_check"   || func_check="check"
+    function_exists "$func_install" || func_install="install"
+    function_exists "$func_version" || func_version="get_version"
+
+    T="Installed"
+    if $func_check "$name"; then
+        T="Already installed"
+    elif [ $(confirm "Do you want to install $dname?" "$is_recommend") = "y" ]; then
+        $func_install "$name"
         if [ $? -ne 0 ]; then
-            log_error "The package is NOT installed: $PACKAGE"
+            log_error "NOT installed: $dname"
             return 1
         fi
     else
         return 0 # user pressed "n"
     fi
     
-    VERSION="$($PACKAGE --version | head -n 1)"
-    log_info "The package is$T installed: $PACKAGE \033[1;30m($VERSION)\033[0m"
-    if [ "$T" = "" ]; then echo ; fi
+    log_info "$T: $dname ${DGRAY}($($func_version $name))${NC}"
+    if [ "$T" = "Installed" ]; then echo ; fi
 }
 
 install_script() { # install(download) a script (/usr/local/bin)
-    SCRIPT="$1"
-    RECOMMEND="${2:-y}"
-    DOWNLOAD_URL="$3"
+    name="$1"
+    download_url="$2"
+    is_recommend="${3:-y}"
 
-    SCRIPT_PATH="/usr/local/bin/$SCRIPT"
+    script_path="/usr/local/bin/$name"
 
-    T=""
-    if [ -e "$SCRIPT_PATH" ] ; then
-        T=" already"
-    elif [ $(confirm "Do you want to install $SCRIPT script?" "$RECOMMEND") = "y" ]; then
-        run_command sudo curl -o "$SCRIPT_PATH" -fsSL "$DOWNLOAD_URL"
+    T="Installed"
+    if [ -e "$script_path" ] ; then
+        T="Already installed"
+    elif [ $(confirm "Do you want to install $name script?" "$is_recommend") = "y" ]; then
+        run_command sudo curl -o "$script_path" -fsSL "$download_url"
         if [ $? -ne 0 ]; then
-            log_error "The script is NOT installed: $SCRIPT"
+            log_error "NOT installed: $name"
             return 1
         fi
-        sudo chmod +x "$SCRIPT_PATH"
+        sudo chmod +x "$script_path"
     else
         return 0 # user pressed "n"
     fi
     
-    log_info "The script  is$T installed: $SCRIPT"
-    if [ "$T" = "" ]; then echo ; fi
+    log_info "$T: $name"
+    if [ "$T" = "Installed" ]; then echo ; fi
 }
 
 ################################################################################
 
-install_htop() {
+htop_install() {
     case $(get_distribution) in
         centos) install epel-release; install htop ;;
         *)      install htop ;;
@@ -177,45 +194,39 @@ install_htop() {
 
 ################################################################################
 
-install_oh_my_zsh() {
-    check zsh; if [ $? -ne 0 ]; then return 1; fi # zsh is not installed
+omz_check()   { [ -e ~/.oh-my-zsh ]; }
+omz_version() { git --git-dir ~/.oh-my-zsh/.git --no-pager log -1 --format="%ai"; }
+omz_install() {
+    check zsh || return 1 # zsh is not installed
 
-    T=""
-    if [ -e ~/.oh-my-zsh ]; then
-        T=" already"
-    elif [ $(confirm "Do you want to install oh-my-zsh?" "y") = "y" ]; then
-        # https://github.com/ohmyzsh/ohmyzsh
-        echo -en "\033[1;30m"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh --unattended | cat
-        if [ $? -ne 0 ]; then
-            log_error "The plugin is NOT installed: oh-my-zsh"
-            return 1
-        fi
+    # https://github.com/ohmyzsh/ohmyzsh
+    echo -en "$DGRAY"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh --unattended | cat
+    if [ $? -ne 0 ]; then return 1; fi
 
-        run_command git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        log_info "The plugin is installed: zsh-syntax-highlighting"
+    run_command git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    log_info "Plugin is installed: zsh-syntax-highlighting"
 
-        run_command git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        log_info "The plugin is installed: zsh-autosuggestions"
-        echo
+    run_command git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    log_info "Plugin is installed: zsh-autosuggestions"
+    echo
 
-        sed -i 's/ZSH_THEME="[A-Za-z]*"/ZSH_THEME="agnoster"/' ~/.zshrc
-        sed -i 's/plugins=([A-Za-z]*)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
-        # source ~/.zshrc
-    fi
-
-    VERSION="$(git --git-dir ~/.oh-my-zsh/.git --no-pager log -1 --format='%ai')"
-    log_info "The plugin  is$T installed: oh-my-zsh \033[1;30m($VERSION)\033[0m"
-    if [ "$T" = "" ]; then echo ; fi
+    sed -i 's/ZSH_THEME="[A-Za-z]*"/ZSH_THEME="agnoster"/' ~/.zshrc
+    sed -i 's/plugins=([A-Za-z]*)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+    # source ~/.zshrc
 }
 
 zsh_set_default_shell() {
-    check zsh; if [ $? -ne 0 ]; then return 1; fi # zsh is not installed
+    check zsh || return 1 # zsh is not installed
 
-    if [ "$SHELL" != "$(which zsh)" ]; then
+    if [ "$SHELL" != "/bin/zsh" ] && [ "$SHELL" != "/usr/bin/zsh" ]; then
         if [ $(confirm "Do you want to change default shell to zsh?" "y") = "y" ]; then
             # sudo chsh -s $(which zsh)
-            run_command sudo usermod --shell $(which zsh) $USER
+            if [ -e /bin/zsh ]; then
+                run_command sudo usermod --shell /bin/zsh $USER
+            else
+                run_command sudo usermod --shell $(which zsh) $USER
+            fi
             log_info "The default shell changed to zsh."
             log_info "This setting will not take effect until you log in again."
         fi
@@ -225,8 +236,8 @@ zsh_set_default_shell() {
 
 ################################################################################
 
-install_docker() {
-    echo -en "\033[1;30m"
+docker_install() {
+    echo -en "$DGRAY"
     case $(get_distribution) in
         amzn) sudo yum -y install docker              ;;
         *)    curl -fsSL https://get.docker.com/ | sh ;;
@@ -247,34 +258,29 @@ install_docker() {
     fi
 }
 
-install_docker_compose() {
-    check docker; if [ $? -ne 0 ]; then return 1; fi
+DOCKER_COMPOSE_VERSION="2.2.2"
+compose_check()   { [ -e ~/.docker/cli-plugins ]; }
+compose_version() { docker compose version; }
+compose_install() {
+    check docker || return 1 # docker is not installed
 
-    T=""
-    if [ -e ~/.docker/cli-plugins ]; then
-        T=" already"
-    elif [ $(confirm "Do you want to install docker-compose(v.2)?" "y") = "y" ]; then
-        # https://github.com/docker/compose/releases
-        mkdir -p ~/.docker/cli-plugins/
-        run_command curl -SL "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
-        if [ $? -ne 0 ]; then
-            log_error "The plugin is NOT installed: docker-compose"
-            return 1
-        fi
-        chmod +x ~/.docker/cli-plugins/docker-compose
-    fi
-
-    VERSION="$(docker compose version)"
-    log_info "The plugin  is$T installed: docker-compose \033[1;30m($VERSION)\033[0m"
-    if [ "$T" = "" ]; then echo ; fi
+    # https://github.com/docker/compose/releases
+    mkdir -p ~/.docker/cli-plugins/
+    URL="https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-$(uname -m)"
+    SAVE=~/.docker/cli-plugins/docker-compose
+    run_command curl -SL "$URL" -o "$SAVE"
+    if [ $? -ne 0 ]; then return 1; fi
+    chmod +x "$SAVE"
 }
 
 ################################################################################
 
-install_code_server() {
+code_check()   { check code-server; }
+code_version() { get_version code-server; }
+code_install() {
     CONFIG_FILE=~/.config/code-server/config.yaml
 
-    echo -en "\033[1;30m"
+    echo -en "$DGRAY"
     curl -fsSL https://code-server.dev/install.sh | sh
     if [ $? -ne 0 ]; then return 1; fi
 
@@ -289,12 +295,10 @@ install_code_server() {
     run_command sudo systemctl status code-server@$USER | cat
     run_command cat "$CONFIG_FILE"
 
-    install_code_server_extensions
+    code_install_extensions
 }
 
-install_code_server_extensions() {
-    check code-server; if [ $? -ne 0 ]; then return 1; fi
-
+code_install_extensions() {
     INPUT=$(confirm "Do you want to install some extensions for code-server?" "Yi")
     if [ "$INPUT" = "n" ]; then return 0; fi
 
@@ -332,6 +336,11 @@ EOT
         LINE="$(trim $LINE)"
         if [ "$LINE" != "" ] && [ "${LINE:0:1}" != "#" ]; then
             run_command code-server --install-extension "$LINE"
+            if [ $? -eq 0 ]; then
+                log_info "The extension is installed: $LINE"
+            else
+                log_error "The extension is NOT installed: $LINE"
+            fi
         fi
     done < "$EXT_FILE"
     rm "$EXT_FILE"
@@ -351,11 +360,10 @@ EOT
 
 main() {
     echo "========================================"
-    echo ">>> DeunLee's Init Script (v.1.1.1)"
+    echo ">>> DeunLee's Init Script (v.1.3.0)"
     echo "========================================"
     echo
     
-    sudo true
     run_command uname -mrs
     run_command id
     echo
@@ -370,23 +378,25 @@ main() {
 
     # update
 
-    install_package "htop"        "" "install_htop"
+    install_package "htop"
     install_package "git"
     install_package "gcc"
     install_package "vim"
     install_package "zsh"
-    install_oh_my_zsh
+    install_package "omz"     "oh-my-zsh"
     zsh_set_default_shell
     # sudo apt install fonts-powerline
-    install_package "docker"      "" "install_docker"
-    install_docker_compose
-    install_package "code-server" "" "install_code_server"
-    install_code_server_extensions
-    install_script  "neofetch"    "" "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
+    install_package "docker"
+    install_package "compose" "docker-compose"
+    install_package "code"    "code-server"
+
+
+    install_script  "neofetch"                 "https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch"
+    install_script  "spectre-meltdown-checker" "https://raw.githubusercontent.com/speed47/spectre-meltdown-checker/master/spectre-meltdown-checker.sh"
     echo
     
     if [ ! -e ~/docker ] && [ $(confirm "Do you want to clone deunlee/Docker-Server repository to ~/docker?" "n") = "y" ]; then
-        git clone https://github.com/deunlee/Docker-Server ~/docker
+        run_command git clone https://github.com/deunlee/Docker-Server ~/docker
     fi
 
     log_info "Finished!"
