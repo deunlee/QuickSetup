b22191fc444bd6c51e0fe961e7634061d9b9c5cc
Deun Lee <deunlee@deunlee.com>
2022-01-03 13:14:48 +0900

    Update color variables

--- script/init.sh
+++ script/init.sh
@@ -18,14 +18,20 @@ mkdir -p "$PATH_HTML"
 
 ################################################################################
 
-NC='\033[0m' # No Color
-RED='\033[0;31m'
-DGRAY='\033[1;30m'
+NC="\033[0m" # No Color
+BLACK="\033[0;30m"  ; DGRAY="\033[1;30m"
+RED="\033[0;31m"    ; LRED="\033[1;31m"
+GREEN="\033[0;32m"  ; LGREEN="\033[1;32m"
+YELLOW="\033[0;33m" ; LYELLOW="\033[1;33m"
+BLUE="\033[0;34m"   ; LBLUE="\033[1;34m"
+PURPLE="\033[0;35m" ; LPURPLE="\033[1;35m"
+CYAN="\033[0;36m"   ; LCYAN="\033[1;36m"
+LGRAY="\033[0;37m"  ; WHITE="\033[1;37m"
 
-log_info()  { echo -e "\033[1;32m[INFO]\033[0m $*"  ; }
-log_warn()  { echo -e "\033[1;33m[WARN]\033[0m $*"  ; }
-log_error() { echo -e "\033[1;31m[ERROR]\033[0m $*" ; }
-log_debug() { echo -e "\033[1;35m[DEBUG]\033[0m $*" ; }
+log_info()  { echo -e "${LGREEN}[INFO]${NC} $*"   ; }
+log_warn()  { echo -e "${LYELLOW}[WARN]${NC} $*"  ; }
+log_error() { echo -e "${LRED}[ERROR]${NC} $*"    ; }
+log_debug() { echo -e "${LPURPLE}[DEBUG]${NC} $*" ; }
 
 confirm() {
     # $1 for prompt string, $2 for default answer
